aa9fdd9e9bd531edee08b4be94cceec9ae7c4eae
user-matth <mathuscardoso@gmail.com>
2025-01-21 22:00:15 -0300

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,60 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="relative h-screen w-screen rounded-lg bg-background overflow-x-hidden flex flex-col pb-24">
+      <div className="w-full h-16 border-b border-neutral-100 py-6">
+        <div className="max-w-screen-lg mx-auto flex items-center justify-between h-full">
+          <div className="flex flex-row gap-2 items-center">
+            <Image src='/icon.png' alt='' width={40} height={40} className='w-9 h-9' />
+            <p className='tracking-tight font-medium text-lg'>Ocurrency</p>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Link href={'https://cotacaoglobal.info/'} target='_blank' className='underline underline-offset-4 cursor-pointer flex items-end'>Live exchange</Link>
+            <Link href="/Ocurrency.zip" passHref className='underline underline-offset-4 cursor-pointer flex items-end'>Download</Link>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="max-w-screen-lg flex flex-col space-y-6 mx-auto">
+        <div className="h-[60vh] flex items-center justify-center">
+          <h1 className="text-[60px] medium tracking-tighter text-center">Ocurrency downloads</h1>
+        </div>
+        <div className="grid grid-cols-3 w-full mx-auto">
+          <div className="col-span-1 px-6">
+            <Image src='/48cae9cb4cba45a0987b1daaabe6ed2b2a6a63c1-720x720.png' alt='' width={720} height={720} className='w-full' />
+            <h2 className='text-4xl font-medium tracking-tight mt-6 mb-4'>Desktop app</h2>
+            <Link href="/Ocurrency.zip" passHref className='underline underline-offset-4 cursor-pointer'>Desktop app for macOS</Link>
+            <Button className='mt-4'>Join Windows waitlist!</Button>
+          </div>
+          <div className="col-span-1 px-6">
+            <Image src='/4042e8ef7dbe5609613c705bdaf926cf65f6326b-720x720.png' alt='' width={720} height={720} className='w-full' />
+            <h2 className='text-4xl font-medium tracking-tight mt-6 mb-4'>Mobile app</h2>
+            <Link href={'https://cotacaoglobal.info/'} target='_blank' className='underline underline-offset-4 cursor-pointer'>Mobile PWA app</Link>
+          </div>
+          <div className="col-span-1 px-6">
+            <Image src='/c7bca6c3aaa40f70ce2ff09bbb5e892559379e8f-720x720.png' alt='' width={720} height={720} className='w-full' />
+            <h2 className='text-4xl font-medium tracking-tight mt-6 mb-4'>More content</h2>
+            <p>Available soon... 👀</p>
+          </div>
+          <div className="col-span-3 px-6 py-16">
+            <p className='text-lg text-center'>By downloading a Ocurrency application, you agree that our Terms of Service apply to your use of that application. If you have entered a different agreement with Ocurrency that covers our applications, that agreement will apply instead.</p>
+          </div>
+        </div>
+      </div>
+      <div className="w-full border-t border-neutral-100 py-6">
+        <div className="max-w-screen-lg mx-auto flex items-center justify-between h-full">
+          <div className="flex flex-row gap-2 items-center">
+            <Image src='/icon.png' alt='' width={40} height={40} className='w-9 h-9' />
+            <p className='tracking-tight font-medium text-lg'>Ocurrency</p>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Link href={'https://cotacaoglobal.info/'} target='_blank' className='underline underline-offset-4 cursor-pointer flex items-end'>Live exchange</Link>
+            <Link href={'/'} className='underline underline-offset-4 cursor-pointer flex items-end'>Download</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
